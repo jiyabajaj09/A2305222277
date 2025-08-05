63745988676e1e62b76363415bcfa3737b4741c4
JIYA BAJAJ <jiyabajaj@JIYAs-MacBook-Pro.local>
2025-08-05 12:42:39 +0530

    Removed submodule and added actual Frontend Test Submission files

---
+++ Frontend Test Submission/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>💙 Affordmed Frontend Test Submission 💙</h1>
+        <p><strong> Submitted by:</strong> Jiya Bajaj</p>
+        <p><strong> Description:</strong> This is a submission for the Affordmed Campus Hiring Frontend Track.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
